cb2aedb7530c4350b556f8a0fad179279dcc69b4
Bob (KochurovaVA) <bob@example.com>
2020-04-05 09:07:37 +0300

    code: вывод минимума

--- main.cpp
+++ main.cpp
@@ -16,22 +16,19 @@ poisk[0]=a+b;
 poisk[1]=a-b;
 poisk[2]=a*b;
 poisk[3]=a/b;
-int max=poisk[0];
-int min=poisk[0];
+double max=poisk[0];
+double min=poisk[0];
     for(size_t i=0;i<3;i++)
-    {
         if (max<poisk[i+1])
         {
             max=poisk[i+1];
         }
-            if (min<poisk[i+1])
-            {
-            min=poisk[i+1];
-            }
-    }
+        if (min>poisk[i + 1])
+		{
+			min=poisk[i + 1];
+		}
+
 cout <<"max="<<max<<'\n';
-cout <<"min="<<min<<'\n';
+cout << "min=" << min '\n'
 return 0;
-}
-
-
+    }
