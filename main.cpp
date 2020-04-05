a6c754079b238dd209b4ff8f103859f16dc8b953
Alice (KochurovaVA) <alice@example.com>
2020-04-05 22:04:23 +0300

    code: поиск минимума

--- main.cpp
+++ main.cpp
@@ -16,15 +16,21 @@ poisk[0]=a+b;
 poisk[1]=a-b;
 poisk[2]=a*b;
 poisk[3]=a/b;
-double max=poisk[0];
+int max=poisk[0];
+int min=poisk[0];
     for(size_t i=0;i<3;i++)
     {
         if (max<poisk[i+1])
         {
             max=poisk[i+1];
         }
+            if (min<poisk[i+1])
+            {
+            min=poisk[i+1];
+            }
     }
 cout <<"max="<<max<<'\n';
+cout <<"min="<<min<<'\n';
 return 0;
 }
 
