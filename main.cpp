2dbf54944d35d1f4778e6b5c28f001201a0bb5b3
Alice (KochurovaVA) <alice@example.com>
2020-04-06 14:45:03 +0300

    code: замена на вещественный тип

--- main.cpp
+++ main.cpp
@@ -17,14 +17,21 @@ poisk[1]=a-b;
 poisk[2]=a*b;
 poisk[3]=a/b;
 double max=poisk[0];
+double min=poisk[0];
+
     for(size_t i=0;i<3;i++)
     {
         if (max<poisk[i+1])
         {
             max=poisk[i+1];
         }
+        if (min>poisk[i+1])
+        {
+            min=poisk[i+1];
+        }
     }
 cout <<"max="<<max<<'\n';
+cout <<"min="<<min<<'\n';
 return 0;
 }
 
